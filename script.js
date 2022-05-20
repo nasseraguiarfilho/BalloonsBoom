204f672311336f03f613c92a87bd175eec345a10
Pedro Nasser <nasseraguiarfilho@gmail.com>
2022-05-19 19:58:46 -0600

    Game is starting to present some shape. You can choose the level and gets redirected for th game page. Still no interaction but it will be the next step.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function startGame() {
+function startingPage() {
+
     var gameLevel = document.getElementById("gameLevel").value;
 
     if (gameLevel == 0) {
@@ -10,3 +11,51 @@ function startGame() {
 
 
 }
+
+
+function startGame() {
+
+    var url = window.location.search;
+    var gameLevel = url.replace("?", "");
+    console.log(gameLevel);
+    var gameTime = 0;
+    var fullBalloons = 0;
+    var emptyBalloons = 0;
+
+    if (gameLevel == 1) {
+        gameTime = 30;
+        fullBalloons = 10;
+    }
+    if (gameLevel == 2) {
+        gameTime = 20;
+        fullBalloons = 20;
+    }
+
+    if (gameLevel == 3) {
+        gameTime = 10;
+        fullBalloons = 30;
+    }
+    //timer
+    document.getElementById('timer').innerHTML = gameTime;
+    createBalloons(fullBalloons, gameLevel);
+
+    //full balloons
+    document.getElementById("full").innerHTML = fullBalloons;
+
+    //empty balloons
+    document.getElementById("empty").innerHTML = emptyBalloons;
+
+
+    function createBalloons(fullBalloons, gameLevel) {
+        for (let area = 1; area <= gameLevel; area++) {
+            for (let i = 0; i < 10; i++) {
+                var balloon = document.createElement("img");
+                balloon.src = "imagens/balao_azul_pequeno.png";
+                balloon.style.margin = "10px";
+                document.getElementById("balloonsArea" + area).appendChild(balloon);
+            }
+        }
+
+    }
+}
+
